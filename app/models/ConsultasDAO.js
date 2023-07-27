8548908cd4dbebed4cbf8836351f69f99e6a7478
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2023-07-27 17:52:27 -0300

    teste

--- app/models/ConsultasDAO.js
+++ app/models/ConsultasDAO.js
@@ -4,6 +4,7 @@ function ConsultasDAO(connection){
 
 ConsultasDAO.prototype.getTeste = async function(callback){
    await this._connection('select codigo_grupo,codigo_cota,versao,cgc_cpf_cliente from cotas where codigo_grupo = 63 and codigo_cota = 23 and versao = 00', callback);
+   console.log(result);
 };
 
 module.exports = function(){
