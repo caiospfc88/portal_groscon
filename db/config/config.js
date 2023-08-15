551fed26f1241f24addf0e24eee489a86be8fbf9
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2023-08-15 14:10:50 -0300

    Implantação do sequelize no projeto

---
+++ db/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": 1035,
+    "database": "portal_groscon",
+    "host": "192.168.201.108",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": 1035,
+    "database": "portal_groscon",
+    "host": "192.168.201.108",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": 1035,
+    "database": "portal_groscon",
+    "host": "192.168.201.108",
+    "dialect": "mysql"
+  }
+}
