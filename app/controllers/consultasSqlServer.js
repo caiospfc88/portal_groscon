410b4bc9662795097d5947111c3339a121bcc8f7
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-03-14 17:52:06 -0300

    Implementação da função de formatação de resposta da consulta sql de comissões

--- app/controllers/consultasSqlServer.js
+++ app/controllers/consultasSqlServer.js
@@ -5,58 +5,238 @@ module.exports.comissoesComReducao = async function (application, req, res) {
   var consultaModel = new application.app.models.ConsultasDAO(connection);
 
   var resConsulta = await consultaModel.getComissoesComReducao(req);
-  //console.log(resConsulta);
-  res.send(resConsulta);
-  //res.render('home/comissoes',{selectTeste : resConsulta});
+  console.log(application.app.utils);
+  var comissao = application.app.utils.formataComissao(req, resConsulta);
+  res.send(comissao);
 };
 
 module.exports.comissoesSemReducao = async function (application, req, res) {
   var connection = application.config.dbConnection;
   var consultaModel = new application.app.models.ConsultasDAO(connection);
   let comissao = new Array();
-  comissao = [[], {}];
-  function ItemComissao() {
+  comissao = [[], [], []];
+  function ItemComissao(
+    GRUPO,
+    COTA,
+    VS,
+    DT_CONT,
+    COD_EQ,
+    COD_REP,
+    TIPO,
+    TAB_COMISSAO,
+    NUM_PARCELA,
+    VAL_TAXA,
+    VAL_BEM,
+    PERC_COMISSAO,
+    VAL_COMISSAO,
+    TOTAL_VENDAS,
+    MAX_COMISSAO
+  ) {
     this.GRUPO = GRUPO;
     this.COTA = COTA;
     this.VS = VS;
     this.DT_CONT = DT_CONT;
     this.COD_EQ = COD_EQ;
     this.COD_REP = COD_REP;
+    this.TIPO = TIPO;
+    this.TAB_COMISSAO = TAB_COMISSAO;
+    this.NUM_PARCELA = NUM_PARCELA;
+    this.VAL_TAXA = VAL_TAXA;
+    this.VAL_BEM = VAL_BEM;
+    this.PERC_COMISSAO = PERC_COMISSAO;
+    this.VAL_COMISSAO = VAL_COMISSAO;
+    this.TOTAL_VENDAS = TOTAL_VENDAS;
+    this.MAX_COMISSAO = MAX_COMISSAO;
   }
-  comissao.valTotalComissao = 0;
-
-  var n = 0;
+  var valorTotal = new Object();
+  valorTotal.valTotalComissao = 0;
+  valorTotal.qtdCotas = 0;
 
   var resConsulta = await consultaModel.getComissoesSemReducao(req);
   //console.log(resConsulta[0]);
 
   if (req.query.opcao == 2) {
     resConsulta[0].forEach((i) => {
-      console.log(i["N2_COD"]);
-
-      if ((i["N1_COD"] = !null && i["VAL_N1"] > 0)) {
-        console.log(i["N2_COD"]);
-        (comissao.valTotalComissao += i["VAL_N1"]),
-          (comissao[0][n] = i["GRUPO"]),
-          n++;
+      if (i["N1_COD"] !== null && i["VAL_N1"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N1"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N1_COD"],
+          i["N1_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N1"],
+          i["VAL_N1"],
+          i["TOTAL_VENDAS_MES"],
+          i["N1_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N2_COD"] !== null && i["VAL_N2"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N2"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N2_COD"],
+          i["N2_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N2"],
+          i["VAL_N2"],
+          i["TOTAL_VENDAS_MES"],
+          i["N2_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N3_COD"] !== null && i["VAL_N3"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N3"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N3_COD"],
+          i["N3_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N3"],
+          i["VAL_N3"],
+          i["TOTAL_VENDAS_MES"],
+          i["N3_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N4_COD"] !== null && i["VAL_N4"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N4"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N4_COD"],
+          i["N4_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N4"],
+          i["VAL_N4"],
+          i["TOTAL_VENDAS_MES"],
+          i["N4_MAXIMO"]
+        );
+        comissao[0].push(item);
       }
     });
+    comissao[1].push(resConsulta[1][0]);
+    comissao[2].push(valorTotal);
   } else {
     resConsulta[0].forEach((i) => {
-      console.log("fora do if", resConsulta[0][i]);
-
-      if (i["N2_COD"] !== null && i["VAL_N2"] > 0) {
-        console.log(i["GRUPO"], n);
-        console.log(i["COTA"], n);
-        console.log(i["VAL_N2"], n);
-        comissao.valTotalComissao = comissao.valTotalComissao + i["VAL_N2"];
-        comissao[0]["GRUPO"] = i["GRUPO"];
-        n++;
+      if (i["N1_COD"] == req.query.codigo && i["VAL_N1"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N1"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N1_COD"],
+          i["N1_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N1"],
+          i["VAL_N1"],
+          i["TOTAL_VENDAS_MES"],
+          i["N1_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N2_COD"] == req.query.codigo && i["VAL_N2"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N2"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N2_COD"],
+          i["N2_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N2"],
+          i["VAL_N2"],
+          i["TOTAL_VENDAS_MES"],
+          i["N2_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N3_COD"] == req.query.codigo && i["VAL_N3"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N3"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N3_COD"],
+          i["N3_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N3"],
+          i["VAL_N3"],
+          i["TOTAL_VENDAS_MES"],
+          i["N3_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N4_COD"] == req.query.codigo && i["VAL_N4"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N4"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N4_COD"],
+          i["N4_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N4"],
+          i["VAL_N4"],
+          i["TOTAL_VENDAS_MES"],
+          i["N4_MAXIMO"]
+        );
+        comissao[0].push(item);
       }
     });
+    comissao[1].push(resConsulta[1][0]);
+    comissao[2].push(valorTotal);
   }
-  console.log("comissão", comissao);
-  res.send(resConsulta);
+  res.send(comissao);
 };
 
 module.exports.quitados = async function (application, req, res) {
--- app/utils/comissaoFormat.js
+++ app/utils/comissaoFormat.js
@@ -1,21 +1,223 @@
-function formattedData(params) {
-  const dataFormatted = params.map((item) => {
-    const datas = item
-      .filter((itemParam) => {
-        if (!!itemParam && !!itemParam.GRUPO) {
-          return true;
-        } else {
-          return false;
-        }
-      })
-      .map((itemParam) => [
-        { GRUPO: itemParam.GRUPO },
-        {},
-        { valTotalComissao: itemParam.VAL_N2 },
-      ]);
-    console.log(datas);
-    return datas;
-  });
-  console.log(dataFormatted);
-}
-module.exports = formattedData();
+module.exports = function formataComissao(req, resConsulta) {
+  let comissao = new Array();
+  comissao = [[], [], []];
+  function ItemComissao(
+    GRUPO,
+    COTA,
+    VS,
+    DT_CONT,
+    COD_EQ,
+    COD_REP,
+    TIPO,
+    TAB_COMISSAO,
+    NUM_PARCELA,
+    VAL_TAXA,
+    VAL_BEM,
+    PERC_COMISSAO,
+    VAL_COMISSAO,
+    TOTAL_VENDAS,
+    MAX_COMISSAO
+  ) {
+    this.GRUPO = GRUPO;
+    this.COTA = COTA;
+    this.VS = VS;
+    this.DT_CONT = DT_CONT;
+    this.COD_EQ = COD_EQ;
+    this.COD_REP = COD_REP;
+    this.TIPO = TIPO;
+    this.TAB_COMISSAO = TAB_COMISSAO;
+    this.NUM_PARCELA = NUM_PARCELA;
+    this.VAL_TAXA = VAL_TAXA;
+    this.VAL_BEM = VAL_BEM;
+    this.PERC_COMISSAO = PERC_COMISSAO;
+    this.VAL_COMISSAO = VAL_COMISSAO;
+    this.TOTAL_VENDAS = TOTAL_VENDAS;
+    this.MAX_COMISSAO = MAX_COMISSAO;
+  }
+  var valorTotal = new Object();
+  valorTotal.valTotalComissao = 0;
+  valorTotal.qtdCotas = 0;
+
+  if (req.query.opcao == 2) {
+    resConsulta[0].forEach((i) => {
+      if (i["N1_COD"] !== null && i["VAL_N1"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N1"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N1_COD"],
+          i["N1_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N1"],
+          i["VAL_N1"],
+          i["TOTAL_VENDAS_MES"],
+          i["N1_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N2_COD"] !== null && i["VAL_N2"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N2"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N2_COD"],
+          i["N2_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N2"],
+          i["VAL_N2"],
+          i["TOTAL_VENDAS_MES"],
+          i["N2_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N3_COD"] !== null && i["VAL_N3"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N3"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N3_COD"],
+          i["N3_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N3"],
+          i["VAL_N3"],
+          i["TOTAL_VENDAS_MES"],
+          i["N3_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N4_COD"] !== null && i["VAL_N4"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N4"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N4_COD"],
+          i["N4_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N4"],
+          i["VAL_N4"],
+          i["TOTAL_VENDAS_MES"],
+          i["N4_MAXIMO"]
+        );
+        comissao[0].push(item);
+      }
+    });
+    comissao[1].push(resConsulta[1][0]);
+    comissao[2].push(valorTotal);
+  } else {
+    resConsulta[0].forEach((i) => {
+      if (i["N1_COD"] == req.query.codigo && i["VAL_N1"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N1"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N1_COD"],
+          i["N1_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N1"],
+          i["VAL_N1"],
+          i["TOTAL_VENDAS_MES"],
+          i["N1_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N2_COD"] == req.query.codigo && i["VAL_N2"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N2"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N2_COD"],
+          i["N2_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N2"],
+          i["VAL_N2"],
+          i["TOTAL_VENDAS_MES"],
+          i["N2_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N3_COD"] == req.query.codigo && i["VAL_N3"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N3"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N3_COD"],
+          i["N3_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N3"],
+          i["VAL_N3"],
+          i["TOTAL_VENDAS_MES"],
+          i["N3_MAXIMO"]
+        );
+        comissao[0].push(item);
+      } else if (i["N4_COD"] == req.query.codigo && i["VAL_N4"] > 0) {
+        valorTotal.valTotalComissao = valorTotal.valTotalComissao + i["VAL_N4"];
+        valorTotal.qtdCotas = valorTotal.qtdCotas + 1;
+        var item = new ItemComissao(
+          i["GRUPO"],
+          i["COTA"],
+          i["VS"],
+          i["DT_CONT"],
+          i["COD_EQ"],
+          i["N4_COD"],
+          i["N4_DESCRICAO"],
+          i["COD_TAB_COM"],
+          i["NUM_PAR"],
+          i["VL_TX_ADM"],
+          i["VL_BEM"],
+          i["PERC_COM_N4"],
+          i["VAL_N4"],
+          i["TOTAL_VENDAS_MES"],
+          i["N4_MAXIMO"]
+        );
+        comissao[0].push(item);
+      }
+    });
+    comissao[1].push(resConsulta[1][0]);
+    comissao[2].push(valorTotal);
+  }
+  return comissao;
+};
