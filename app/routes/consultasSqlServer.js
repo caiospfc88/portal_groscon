0cffd5e3b99ed54da5b60c6582df4c1f8c736577
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-04-01 10:20:11 -0300

    criada a validação de token jwt

--- app/routes/consultasSqlServer.js
+++ app/routes/consultasSqlServer.js
@@ -1,25 +1,46 @@
+const { verifyJWT } = require("../utils/auth");
 
-
-module.exports = function(application){
-        application.get('/comissoesSemReducao', function(req, res){        
-        application.app.controllers.consultasSqlServer.comissoesSemReducao(application, req, res);
-    });
-        application.get('/comissoesComReducao', function(req, res){        
-        application.app.controllers.consultasSqlServer.comissoesComReducao(application, req, res);
-    });
-        application.get('/quitados', function(req, res){        
-        application.app.controllers.consultasSqlServer.quitados(application, req, res);
-    });
-        application.get('/aniversariantesMes', function(req, res){        
-        application.app.controllers.consultasSqlServer.aniversariantesMes(application, req, res);
-    });
-        application.get('/relatorioRenegociacoes', function(req, res){        
-        application.app.controllers.consultasSqlServer.relatorioRenegociacoes(application, req, res);
-    });
-        application.get('/relatorioAproveitamento', function(req, res){        
-        application.app.controllers.consultasSqlServer.relatorioAproveitamento(application, req, res);
-    });
+module.exports = function (application) {
+  application.get("/comissoesSemReducao", verifyJWT, function (req, res) {
+    application.app.controllers.consultasSqlServer.comissoesSemReducao(
+      application,
+      req,
+      res
+    );
+  });
+  application.get("/comissoesComReducao", function (req, res) {
+    application.app.controllers.consultasSqlServer.comissoesComReducao(
+      application,
+      req,
+      res
+    );
+  });
+  application.get("/quitados", function (req, res) {
+    application.app.controllers.consultasSqlServer.quitados(
+      application,
+      req,
+      res
+    );
+  });
+  application.get("/aniversariantesMes", function (req, res) {
+    application.app.controllers.consultasSqlServer.aniversariantesMes(
+      application,
+      req,
+      res
+    );
+  });
+  application.get("/relatorioRenegociacoes", function (req, res) {
+    application.app.controllers.consultasSqlServer.relatorioRenegociacoes(
+      application,
+      req,
+      res
+    );
+  });
+  application.get("/relatorioAproveitamento", function (req, res) {
+    application.app.controllers.consultasSqlServer.relatorioAproveitamento(
+      application,
+      req,
+      res
+    );
+  });
 };
-
-
-       
--- app/utils/auth.js
+++ app/utils/auth.js
@@ -1,30 +1,22 @@
-require("dotenv-safe").config();
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 
-async function criptografaSenha(senha) {
-  await bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash(senha, salt, function (err, hash) {
-      console.log("hash", hash);
-      return hash;
-      // Store hash in your password DB.
-    });
-  });
-}
 async function verifyJWT(req, res, next) {
   const token = req.headers["authorization"];
   if (!token)
-    return res.status(401).json({ auth: false, message: "No token provided." });
+    return res
+      .status(401)
+      .json({ auth: false, message: "Token não fornecido." });
 
   jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
     if (err)
       return res
         .status(500)
-        .json({ auth: false, message: "Failed to authenticate token." });
+        .json({ auth: false, message: "Falha na autenticação do token." });
 
     // se tudo estiver ok, salva no request para uso posterior
     req.userId = decoded.id;
     next();
   });
 }
-module.exports = { criptografaSenha, verifyJWT };
+module.exports = { verifyJWT };
