ab24d9b8d2dc8fe89efc13c11f3d9a899995237a
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-04-08 18:00:00 -0300

    Alteração em função de gerar planilha

--- app/utils/geraPlanilha.js
+++ app/utils/geraPlanilha.js
@@ -1,11 +1,33 @@
 const xl = require("excel4node");
 
 function geraPlanilhaBradesco(obj) {
-  /*const wb = new xl.Workbook();
-  const ws = wb.addWorkSheet("Teste");*/
-  let valor = [];
-  valor = Object.values(obj);
-  console.log("valor", valor);
-  return valor;
+  const wb = new xl.Workbook();
+  const ws = wb.addWorksheet("Primeira Aba");
+  var values = [];
+  var colunas = new Array();
+  /*Object.entries(obj[0]).map(([chave, valor]) =>
+    colunas.push({ header: chave, key: chave })
+  );
+  obj.forEach((element) => {
+    values.push({Object.values(element)});
+  });
+  sheet.columns = colunas;*/
+  Object.entries(obj[0]).map(([chave, valor]) => colunas.push(chave));
+  let colunaIndex = 1;
+  colunas.forEach((heading) => {
+    ws.cell(1, colunaIndex++).string(heading);
+  });
+  let linhaIndex = 2;
+  obj.forEach((record) => {
+    let colunaIndex = 1;
+    Object.keys(record).forEach((colunaNome) => {
+      ws.cell(linhaIndex, colunaIndex++).string(record[colunaNome]);
+    });
+    linhaIndex++;
+  });
+
+  wb.write("teste.xls");
+  console.log(colunas);
+  //console.log("Valores", values);
 }
 module.exports = { geraPlanilhaBradesco };
