b1d4968157ac6668d95f0675e16434846e981db3
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-05-02 17:12:56 -0300

    adicionando permissão em site através do cabeçalho

--- config/server.js
+++ config/server.js
@@ -15,6 +15,15 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
