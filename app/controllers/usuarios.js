ff968e1db543ecea0b87f6d583cef23d8582e5ce
Caio Vinicius <caiospfc88@gmail.com>
2024-11-27 10:10:40 -0300

    atualização da logica da query do controller listarUsuariosAniverPeriodo para listar corretamente os usuarios de acordo com o filtro de data enviado

--- app/controllers/usuarios.js
+++ app/controllers/usuarios.js
@@ -28,53 +28,92 @@ module.exports.listarUsuarios = async function (req, res) {
 };
 
 module.exports.listarUsuariosAniverPeriodo = async function (req, res) {
-  console.log(req.query.data_inicial,req.query.data_final)
+  console.log("users niver params: ", req.query.data_inicial, req.query.data_final);
+
+  // Extração e conversão de datas
   const [mesInicial, diaInicial] = req.query.data_inicial.split('-').map(Number);
   const [mesFinal, diaFinal] = req.query.data_final.split('-').map(Number);
-  const aniversariantes = await models.usuarios.findAll({
-    attributes: [   
-      ['nome', 'NOME'],     
-      [Sequelize.fn('DATE_FORMAT', Sequelize.col('data_nascimento'), '%d/%m/%Y'), 'DATA_NASCIMENTO'],
-      ['email','E_MAIL'],
-    ],
-    where: {
-      [Op.or]: [
-        // Aniversariantes no mesmo ano
-        {
-          [Op.and]: [
-            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), {
-              [Op.eq]: mesInicial,
-            }),
-            Sequelize.where(Sequelize.fn('DAY', Sequelize.col('data_nascimento')), {
-              [Op.gte]: diaInicial,
-            }),
-          ],
-        },
-        {
-          [Op.and]: [
-            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), {
-              [Op.eq]: mesFinal,
-            }),
-            Sequelize.where(Sequelize.fn('DAY', Sequelize.col('data_nascimento')), {
-              [Op.lte]: diaFinal,
-            }),
-          ],
-        },
-        // Aniversariantes entre os meses
-        {
-          [Op.and]: [
-            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), {
-              [Op.gt]: mesInicial,
-            }),
-            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), {
-              [Op.lt]: mesFinal,
-            }),
-          ],
-        },
+  console.log("Mes Inicial:", mesInicial, "Dia Inicial:", diaInicial);
+  console.log("Mes Final:", mesFinal, "Dia Final:", diaFinal);
+
+  let whereCondition;
+
+  if (mesInicial === mesFinal && diaInicial === diaFinal) {
+    // Caso 1: Intervalo de um único dia
+    whereCondition = {
+      [Op.and]: [
+        Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), mesInicial),
+        Sequelize.where(Sequelize.fn('DAY', Sequelize.col('data_nascimento')), diaInicial),
       ],
-    },
-  });
-  res.send(aniversariantes);
+    };
+  } else if (mesInicial > mesFinal) {
+    // Caso 2: Intervalo cruzando o ano
+    whereCondition = {
+      [Op.or]: [
+        // Parte 1: Meses no final do ano
+        {
+          [Op.and]: [
+            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), { [Op.gte]: mesInicial }),
+            Sequelize.where(Sequelize.fn('DAY', Sequelize.col('data_nascimento')), { [Op.gte]: diaInicial }),
+          ],
+        },
+        // Parte 2: Meses no início do ano
+        {
+          [Op.and]: [
+            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), { [Op.lte]: mesFinal }),
+            Sequelize.where(Sequelize.fn('DAY', Sequelize.col('data_nascimento')), { [Op.lte]: diaFinal }),
+          ],
+        },
+        // Parte 3: Meses intermediários
+        Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), {
+          [Op.or]: [
+            { [Op.gt]: mesInicial },
+            { [Op.lt]: mesFinal },
+          ],
+        }),
+      ],
+    };
+  } else {
+    // Caso 3: Intervalo normal dentro do mesmo ano
+    whereCondition = {
+      [Op.or]: [
+        // Mês inicial
+        {
+          [Op.and]: [
+            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), mesInicial),
+            Sequelize.where(Sequelize.fn('DAY', Sequelize.col('data_nascimento')), { [Op.gte]: diaInicial }),
+          ],
+        },
+        // Mês final
+        {
+          [Op.and]: [
+            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), mesFinal),
+            Sequelize.where(Sequelize.fn('DAY', Sequelize.col('data_nascimento')), { [Op.lte]: diaFinal }),
+          ],
+        },
+        // Meses intermediários
+        Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('data_nascimento')), { [Op.gt]: mesInicial, [Op.lt]: mesFinal }),
+      ],
+    };
+  }
+
+  try {
+    // Execução da query com condições calculadas
+    const aniversariantes = await models.usuarios.findAll({
+      attributes: [
+        ['nome', 'NOME'],
+        [Sequelize.fn('DATE_FORMAT', Sequelize.col('data_nascimento'), '%d/%m/%Y'), 'DATA_NASCIMENTO'],
+        ['email', 'E_MAIL'],
+      ],
+      where: whereCondition,
+    });
+
+    console.log("Aniversariantes: ", aniversariantes);
+    res.send(aniversariantes);
+  } catch (error) {
+    console.error("Erro ao listar aniversariantes:", error);
+    res.status(500).send({ error: "Erro ao listar aniversariantes" });
+  }
 };
 
 module.exports.consultarUsuario = async function (req, res) {
